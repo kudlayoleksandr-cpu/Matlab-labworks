066c16687a1eea9ac34569d656c00880553a28d7
Oleksandr Kudlai <kudlayoleksandr@gmail.com>
2026-09-14 09:46:29 +0300

    Add files via upload

---
+++ LW 1/LWOleksandr.m
@@ -0,0 +1,27 @@
+% Paprastai skriptas
+% Oleksandr Kudlai Edlfu 25/2 14.09.2026
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X ašis')
+ylabel('Y_2 [--]   Y_2 [--]')
+%% Complementary task
+N = 2; 
+
+v = N+1:0.5:N+4;
+
+% 3. Create matrix A
+A = N:N+8;
+A = reshape(A, 3, 3)';
+
+% 4. Select green elements
+a = A(3,2);
+b = A(2:3,1:2);
+c = [A(1,1) A(1,3); A(3,1) A(3,3)];
+
+% 5. Concatenate A with vector
+v2 = v(1:3)';
+B = [A v2];
